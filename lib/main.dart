9b454f64849466502a7ddf038f500209c4c4110b
lmh <leemh332@gmail.com>
2024-10-11 14:14:34 +0900

    add main func

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,262 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:todoredo/models/chat.model.dart';
+import 'package:todoredo/providers/providers.dart';
+import 'package:todoredo/util/weekday_convertor.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MainPage extends HookConsumerWidget {
+  const MainPage({super.key});
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final chat = ref.watch(chatProvider);
+    final controller = useTextEditingController();
+    final scrollController = useScrollController();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    ref.listen(
+      chatProvider.notifier,
+      (previous, next) {
+        Future.delayed(
+          const Duration(milliseconds: 10),
+          () {
+            scrollController.animateTo(
+                scrollController.position.maxScrollExtent,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeOut);
+          },
+        );
+      },
+    );
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: const Color(0xffeeeeee),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.white,
+        surfaceTintColor: Colors.white,
+        title: const Text('logo'),
+        centerTitle: false,
+        actions: const [],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Expanded(
+              child: chat.maybeWhen(
+                data: (data) {
+                  return ListView.builder(
+                    controller: scrollController,
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      final todo = data[index];
+                      final date = DateFormat('MM. dd').format(todo.date);
+                      return Column(
+                        children: [
+                          if (index == 0)
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8),
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 12, vertical: 8),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Text(
+                                    '$date ${weekdayConvertor(todo.date.weekday)}'),
+                              ),
+                            )
+                          else if (date !=
+                              DateFormat('MM. dd').format(data[index - 1].date))
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8),
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 12, vertical: 8),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Text(
+                                    '$date ${weekdayConvertor(todo.date.weekday)}'),
+                              ),
+                            ),
+                          Chat(todo: todo),
+                        ],
+                      );
+                    },
+                  );
+                },
+                orElse: () => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            BottomWidget(controller: controller),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class BottomWidget extends HookConsumerWidget {
+  const BottomWidget({
+    super.key,
+    required this.controller,
+  });
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectRe = useState(false);
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+      child: TextFormField(
+        controller: controller,
+        onFieldSubmitted: (value) {
+          ref.read(chatProvider.notifier).addchat(value, selectRe.value);
+        },
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          prefixIcon: InkWell(
+            borderRadius: BorderRadius.circular(20),
+            onTap: () {
+              selectRe.value = !selectRe.value;
+            },
+            child: Icon(
+              Icons.restart_alt,
+              color: selectRe.value ? Colors.red : Colors.grey,
+            ),
+          ),
+          suffixIcon: InkWell(
+            borderRadius: BorderRadius.circular(20),
+            onTap: () {
+              ref
+                  .read(chatProvider.notifier)
+                  .addchat(controller.text, selectRe.value);
+            },
+            child: const Icon(Icons.send),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Chat extends HookConsumerWidget {
+  final Todo todo;
+  const Chat({
+    super.key,
+    required this.todo,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+      child: InkWell(
+        onLongPress: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return EditChatDialog(todo);
+            },
+          );
+        },
+        onDoubleTap: () {
+          ref.read(chatProvider.notifier).completeChat(todo.id);
+        },
+        child: Row(
+          mainAxisAlignment:
+              todo.redo ? MainAxisAlignment.start : MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: todo.complete
+                      ? Colors.green.withOpacity(0.8)
+                      : Colors.white),
+              child: Text(
+                todo.title,
+                style: TextStyle(
+                    color: todo.complete ? Colors.white : Colors.black),
+              ),
+            ),
+            const SizedBox(width: 4),
+            // Text(
+            //   '${todo.date.hour}:${todo.date.minute}',
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EditChatDialog extends HookConsumerWidget {
+  final Todo todo;
+  const EditChatDialog(this.todo, {super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = useTextEditingController(text: todo.title);
+    return AlertDialog(
+      title: const Text('Edit Todo'),
+      content: TextField(
+        controller: controller,
+        autofocus: true,
+      ),
+      actions: [
+        TextButton(
+            onPressed: () {
+              ref.read(chatProvider.notifier).deleteChat(todo.id);
+              Navigator.pop(context);
+            },
+            child: const Text('삭제')),
+        TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('취소')),
+        TextButton(
+            onPressed: () {
+              ref
+                  .read(chatProvider.notifier)
+                  .editChat(todo.id, controller.text);
+              Navigator.pop(context);
+            },
+            child: const Text('확인')),
+      ],
     );
   }
 }
