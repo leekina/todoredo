2bd59cf1db481e96bc8b59f289d7316d87eee071
lmh <leemh332@gmail.com>
2024-10-11 14:13:56 +0900

    add todo model

---
+++ lib/models/chat.model.dart
@@ -0,0 +1,29 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:uuid/uuid.dart';
+
+part 'chat.model.g.dart';
+part 'chat.model.freezed.dart';
+
+const uuid = Uuid();
+
+@freezed
+class Todo with _$Todo {
+  const factory Todo({
+    required String id,
+    required String title,
+    required DateTime date,
+    @Default(false) bool complete,
+    @Default(false) bool redo,
+  }) = _Todo;
+
+  factory Todo.add({required String todo, required bool re}) {
+    return Todo(
+      id: uuid.v4(),
+      title: todo,
+      date: DateTime.now(),
+      redo: re,
+    );
+  }
+
+  factory Todo.fromJson(Map<String, dynamic> json) => _$TodoFromJson(json);
+}
