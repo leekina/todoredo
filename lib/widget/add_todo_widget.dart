f3ac1d6a374ef8530741591166a166d75f4cc92d
lmh <leemh332@gmail.com>
2024-11-01 23:27:38 +0900

    바텀 바 디자인 변경

--- lib/widget/add_todo_widget.dart
+++ lib/widget/add_todo_widget.dart
@@ -17,19 +17,31 @@ class AddTodoWidget extends HookConsumerWidget {
       decoration: const BoxDecoration(
         color: Colors.white,
       ),
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-      child: TextFormField(
-        controller: controller,
-        focusNode: addTodoNode,
-        onFieldSubmitted: (value) {
-          ref.read(crudTodoProvider.notifier).addTodo(
-                chat: value,
-              );
-          controller.clear();
-        },
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          suffixIcon: InkWell(
+      padding: const EdgeInsets.fromLTRB(20, 8, 20, 0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              controller: controller,
+              focusNode: addTodoNode,
+              onFieldSubmitted: (value) {
+                ref.read(crudTodoProvider.notifier).addTodo(
+                      chat: value,
+                    );
+                controller.clear();
+              },
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+                fillColor: Colors.grey.shade300,
+                filled: true,
+                border: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+              ),
+            ),
+          ),
+          const SizedBox(width: 8),
+          InkWell(
             borderRadius: BorderRadius.circular(20),
             onTap: () {
               ref
@@ -37,9 +49,10 @@ class AddTodoWidget extends HookConsumerWidget {
                   .addTodo(chat: controller.text);
               controller.clear();
             },
-            child: const Icon(Icons.send),
+            child: const CircleAvatar(
+                backgroundColor: Colors.greenAccent, child: Icon(Icons.send)),
           ),
-        ),
+        ],
       ),
     );
   }
